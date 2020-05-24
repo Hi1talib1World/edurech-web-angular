e77c1f03e1d3dc8e09c1d615a6ca4edeb3f010fa
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-24 10:25:43 +0100

    Update side-nav-inner-toolbar.component.ts

--- src/app/shared/layout/side-nav-inner-toolbar/side-nav-inner-toolbar.component.ts
+++ src/app/shared/layout/side-nav-inner-toolbar/side-nav-inner-toolbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, NgModule, Input, ViewChild } from '@angular/core';
 import { SideNavigationMenuModule, HeaderModule } from '../../shared/components';
-import { ScreenService } from '../../shared/services';
 import { DxDrawerModule } from 'devextreme-angular/ui/drawer';
 import { DxScrollViewModule, DxScrollViewComponent } from 'devextreme-angular/ui/scroll-view';
 import { DxToolbarModule } from 'devextreme-angular/ui/toolbar';
