1d2fccfc5cfc3f73f392a9e5f9e99d336b99b976
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-25 08:03:33 +0100

    header component

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,47 @@
+import { Component, NgModule, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxToolbarModule } from 'devextreme-angular/ui/toolbar';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent {
+  @Output()
+  menuToggle = new EventEmitter<boolean>();
+
+  @Input()
+  menuToggleEnabled = false;
+
+  @Input()
+  title: string;
+
+  userMenuItems = [{
+    text: 'Profile',
+    icon: 'user'
+  }, {
+    text: 'Logout',
+    icon: 'runner',
+    
+  }];
+
+
+  toggleMenu = () => {
+    this.menuToggle.emit();
+  }
+}
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DxButtonModule,
+    DxToolbarModule
+  ],
+  declarations: [ HeaderComponent ],
+  exports: [ HeaderComponent ]
+})
+export class HeaderModule { }
